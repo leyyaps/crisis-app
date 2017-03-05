60a025a63a7e1a409ee12036c225d45349661858
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-05 13:11:30 +0000

    Configured image uploaders

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,6 @@
 class Ticket < ApplicationRecord
+
+  mount_uploader :small_image, SmallImageUploader
+  mount_uploader :cover_image, CoverImageUploader
+  mount_uploader :square_image, SquareImageUploader
 end
