a93e7b211d3dacf7b29b994f61d43369e58a3ffe
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-14 16:41:15 +0000

    bug fixing

--- app/controllers/lost_items_controller.rb
+++ app/controllers/lost_items_controller.rb
@@ -8,7 +8,7 @@ class LostItemsController < ApplicationController
     @lost_item = LostItem.new(params[:lost_item])
     @lost_item.request = request
     if @lost_item.deliver
-      redirect_to contact_us_path, notice: "Thank you! Your message has been sent."
+      redirect_to lostproperty_path, notice: "Thank you! Your message has been sent."
     else
       flash.now[:error] = 'Sorry! An error occured. Please try again.'
       render :new
--- app/models/lost_item.rb
+++ app/models/lost_item.rb
@@ -1,5 +1,4 @@
 class LostItem < MailForm::Base
-
   attribute :name,      :validate => true
   attribute :mobile,    :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
