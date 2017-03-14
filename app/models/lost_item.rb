183ba98af7186d02d18db67ea5d07a61a130eceb
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-14 17:30:16 +0000

    validating date on lost item form

--- app/models/lost_item.rb
+++ app/models/lost_item.rb
@@ -3,7 +3,7 @@ class LostItem < MailForm::Base
   attribute :mobile,    :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  attribute :date
+  attribute :date, :validate => true
   attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
