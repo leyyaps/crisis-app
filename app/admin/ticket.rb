dabc0aa2d0a87aa13a7a11863e5f6aa92732d7b3
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-15 10:06:15 +0000

    authorise active admin to access fixr field

--- app/admin/ticket.rb
+++ app/admin/ticket.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Ticket do
 
 
 
-  permit_params :small_image, :cover_image, :square_image, :ticket_link_id, :date, :opening_time, :theme, :description
+  permit_params :small_image, :cover_image, :square_image, :ticket_link_id, :date, :opening_time, :theme, :description, :fixr
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
