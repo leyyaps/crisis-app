8bd2e3e117574ab0885dda39178dba8a6cdcef06
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-16 09:32:00 +0000

    date picker fix

--- app/controllers/lost_items_controller.rb
+++ app/controllers/lost_items_controller.rb
@@ -8,7 +8,7 @@ class LostItemsController < ApplicationController
     @lost_item = LostItem.new(params[:lost_item])
     @lost_item.request = request
     if @lost_item.deliver
-      redirect_to lostproperty_path, notice: "Thank you! Your message has been sent."
+      redirect_to lostproperty_path, notice: "Thank you! Your message has been sent. Please note: It may take up to 48hrs for you to hear anything. If no reply, your item has sadly not been found."
     else
       flash.now[:error] = @lost_item.errors.full_messages
       render :new
