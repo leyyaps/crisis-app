9fd03d1c22cbdaab3222875ca94f4bc3dc3844fe
leyyaps <andrew.smith@ipsofactouk.com>
2017-10-17 10:54:48 +0100

    remove seven legged

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'privacy' => 'static_pages#privacy'
 
-  get 'sevenlegged' => 'static_pages#sevenlegged'
+
 
   get 'loaderio-0a7e4837e88f50cf53f7be88cd053c92' => 'static_pages#loaderio-0a7e4837e88f50cf53f7be88cd053c92.txt'
 
